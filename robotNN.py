9408c81e4e31930759cc2f69f75688cd89d01f50
elenakelly <elena_kane@hotmail.com>
2022-03-04 15:32:32 +0100

    the NN

--- robotNN.py
+++ robotNN.py
@@ -1,23 +1,50 @@
 import robot
 import numpy as np
+import random
 
 class RobotNN():
-    def _init_(self, layers, n_nodes, activation_function):
-        self.layers = layers
-        self.inputLayer = []
-        self.hiddenLayer = []
-        self.outputLayer = []
-        self.n_nodes = n_nodes
-        self.activation_function = activation_function
-    
-    def layers(self):
-        pass
+    def _init_(self, n_nodes, n_neurons):
+        self.num_inputs = 10
+        self.num_hidden = 4
+        self.num_outputs = 2
+        #setting our layers
+        layers = [self.num_inputs] + [self.num_hidden] + [self.num_outputs]
 
+        #random weights and biases
+        weights = []
+        biases = []
+        for i in range(len(layers)-1):
+            w = np.random.rand(layers[i], layers[i+1])
+            weights.append(w)
+            b = np.zeros(layers[i])
+            biases.append(b)
+        self.weights = weights
+        self.biases = biases
     
-    def forward_propagation(self, input_data):
-        self.input = input_data
-        self.output = np.dot(self.input, self.weights) + self.bias
-        return self.output
+        #create activations 
+        activations =[]
+        for i in range(len(layers)):
+            a = np.zeros(layers[i])
+            activations.append(a)
+        self.activations = activations    
+    
+    #implement forwardpropagation
+    def forward_propagate(self,inputs):      
+        # the input layer activation is just the input itself
+        activations = inputs
+        for i, w in enumerate(self.weights):
+            #calculate the net inputs
+            net_inputs = np.dot(activations, w)
+            #calculate the activations
+            #activation of the sigmoid function ==> f(x) = 1/(1+e^(-x)) 
+            activations = 1.0/(1+np.exp(-net_inputs))
+            # save the activations for backpropogation
+            self.activations[i + 1] = activations
+            print(activations)
+        return activations
+        
+
+
  
 
 class RobotEA():
