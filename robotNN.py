6aa4f3df186be050e90dcee9daf213a13a18f2d4
elenakelly <elena_kane@hotmail.com>
2022-03-04 14:16:25 +0100

    runnung

--- robotNN.py
+++ robotNN.py
@@ -1,8 +1,24 @@
 import robot
+import numpy as np
 
 class RobotNN():
-    def __init__(self):
+    def _init_(self, layers, n_nodes, activation_function):
+        self.layers = layers
+        self.inputLayer = []
+        self.hiddenLayer = []
+        self.outputLayer = []
+        self.n_nodes = n_nodes
+        self.activation_function = activation_function
+    
+    def layers(self):
         pass
+
+    
+    def forward_propagation(self, input_data):
+        self.input = input_data
+        self.output = np.dot(self.input, self.weights) + self.bias
+        return self.output
+ 
 
 class RobotEA():
     def __init__(self):
@@ -19,3 +35,7 @@ class RobotEA():
 
     def mutation(self):
         pass
+
+
+#if _name_ == '_main_':
+ #   Network(Layer(10, 'relu'), Layer(4, 'relu'), Layer(2, 'linear'))
