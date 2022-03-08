6de39d788c2ac3c8495d4a2bcde4763c751d89b6
Nikolaos Ntantis <nickd_96@hotmail.com>
2022-03-08 13:01:12 +0100

    Update ffnn.py

--- ffnn.py
+++ ffnn.py
@@ -14,23 +14,24 @@ class network():
             self.act_sigmoid1 = network.Activation_Sigmoid()
             self.output_layer2 = network.layer(4, 2)
             self.act_tanh = network.Activation_Tanh()
+            self.weights = [self.hidden_layer1.weights, self.output_layer2.weights]
         else:
             self.hidden_layer1 = network.layer(16, 4, self.weights[0])
             self.act_sigmoid1 = network.Activation_Sigmoid()
             self.output_layer2 = network.layer(4, 2, self.weights[1])
             self.act_tanh = network.Activation_Tanh()
-
+            self.weights = [self.hidden_layer1.weights, self.output_layer2.weights]
     class layer:
         def __init__(self, n_inputs, n_neurons, weights=None):
             if weights is None:
                 self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-                self.biases = np.zeros((1, n_neurons))
+                #self.biases = np.zeros((1, n_neurons))
             else:
                 self.weights = weights
-                self.biases = np.zeros((1, n_neurons))
+                #self.biases = np.zeros((1, n_neurons))
 
         def forward(self, inputs):
-            self.output = np.dot(inputs, self.weights) + self.biases
+            self.output = np.dot(inputs, self.weights)# + self.biases
 
     class Activation_Tanh:
         def forward(self, inputs):
@@ -49,12 +50,11 @@ class network():
 
         self.act_sigmoid1.forward(self.hidden_layer1.output)
         hidden_layer_output = self.act_sigmoid1.output  # Also feedback
-        self.feedback = hidden_layer_output[0]
+        self.feedback = hidden_layer_output
 
 
         self.output_layer2.forward(self.hidden_layer1.output)
 
         self.act_tanh.forward(self.output_layer2.output)
         motor_output = self.act_tanh.output
-        print(motor_output[0])
-        return motor_output[0], hidden_layer_output
+        return motor_output, hidden_layer_output
