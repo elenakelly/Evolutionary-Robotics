661b9635a5516a8fe307cdaf19ed304c157a18a3
Nikolaos Ntantis <nickd_96@hotmail.com>
2022-03-10 20:36:21 +0100

    Update runRobot.py

--- runRobot.py
+++ runRobot.py
@@ -15,24 +15,24 @@ class RunRobot():
         best_scores = []
         for epoch in range(epochs):
             accumulated_score = 0
-            #print("=============start of epoch=====================")
+            print("=============start of epoch=====================")
             for i in range(len(self.robotEA.population)):
                 #create and simulate robot given the neural network
-                self.robotEA.population[i].update_score(Robot.Robot(self.robotEA.population[i].NN).results)
-                #print("this is the score returned from the simulation", self.robotEA.population[i].score)
+                self.robotEA.population[i].update_score(Robot.Robot(self.robotEA.population[i].NN, epoch, i).results)
+                print("this is the score returned from the simulation", self.robotEA.population[i].score)
                 accumulated_score +=self.robotEA.population[i].score
 
             average_scores.append(accumulated_score/len(self.robotEA.population))
 
             lis = [rob.score for rob in self.robotEA.population]
             lis.sort()
-            #print("Iteration ", epoch, " best score: ", [rob.score for rob in self.robotEA.population])
-            #print("Iteration ", epoch, " best score: ", max(lis))
+            print("Iteration ", epoch, " best score: ", [rob.score for rob in self.robotEA.population])
+            print("Iteration ", epoch, " best score: ", max(lis))
             best_scores.append(max(lis))
 
             # call Evolutionary Algorithms to update the weights and proceed to the next iteration
             self.robotEA.run()
-            #print("=============end of epoch=====================")
+            print("=============end of epoch=====================")
         plt.plot(average_scores, label="Average Scores", c='blue')
         plt.plot(best_scores, label="Best Scores", c='red')
         plt.show()
