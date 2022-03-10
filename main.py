01a8fd79ff3a366e8f6909aca01701175d0def48
Nikolaos Ntantis <nickd_96@hotmail.com>
2022-03-10 20:35:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from runRobot import RunRobot
 
-iterations = 50
+iterations = 100
 
-pop_size = 10
+pop_size = 15
 select_perc = 0.5
 error_range = 0.1
-mutate = 0.1
+mutate = 0.5
 
 runRobot = RunRobot(pop_size, select_perc, error_range, mutate)
 
